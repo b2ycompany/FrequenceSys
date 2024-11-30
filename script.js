7c2f75d370f41df596c966ec1d97b0890f33f82d
b2ycompany <b2ylion@gmail.com>
2024-11-30 10:11:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
+// Script de Animação para Scroll document.querySelector('.cta-button').addEventListener('click', () => { document.querySelector('#about').scrollIntoView({ behavior: 'smooth' }); });
 
